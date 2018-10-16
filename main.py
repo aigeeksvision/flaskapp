aa5e965449f8e24b20fae0a9ce4651a00ac92ea4
Hemil Patel <hemilpatel41196@gmail.com>
2018-10-16 18:34:11 +0530

    [refactor] Code improved and commenting

--- main.py
+++ main.py
@@ -1,17 +1,27 @@
+# Dependencies
 from flask import Flask, render_template
 
+# Flask App
 app = Flask(__name__)
+# Defalut route
 @app.route("/")
+def default():
+   return render_template("layout.html")
+
+# /first route
+@app.route("/first")
 def first():
    return render_template("first.html")
 
+# /second route
 @app.route("/second")
 def second():
    return render_template("second.html")
 
+# /third route
 @app.route("/third")
 def third():
    return render_template("third.html")
 
 if __name__ == '__main__':
-   app.run()
+   app.run(debug=True)
